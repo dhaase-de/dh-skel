2cb1f8e1c1b8e9a741b2b69dda41885bccf62261
Daniel Haase <git@dhaase.de>
2023-03-19 19:45:58 +0100

    Allow multiple arguments (and also individual files as arguments)

--- utils/bin/toshm.py
+++ utils/bin/toshm.py
@@ -10,7 +10,7 @@ def bytes_to_human_str(b: int) -> str:
     prefixes = ["", "k", "M", "G", "T"]
     prefix_count = len(prefixes)
     n_prefix = 0
-    while (b > 1024.0) and (n_prefix + 1 < prefix_count):
+    while (b >= 1024.0) and (n_prefix + 1 < prefix_count):
         b /= 1024.0
         n_prefix += 1
 
@@ -19,7 +19,7 @@ def bytes_to_human_str(b: int) -> str:
 
 def get_args():
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument("source_path", type=str, help="Path which should be copied recursively.")
+    parser.add_argument("source_paths", metavar="source_path", nargs="+", type=str, help="Path(s) which should be copied recursively.")
     parser.add_argument("-t", "--target-base-path", type=str, default="/dev/shm", help="Target base path.")
     parser.add_argument("-d", "--debug", action="store_true", help="Display full stack trace on error.")
     args = parser.parse_args()
@@ -27,17 +27,21 @@ def get_args():
 
 
 def main(args):
-    source_path = pathlib.Path(args.source_path)
+    source_paths = tuple(pathlib.Path(path) for path in args.source_paths)
     target_base_path = pathlib.Path(args.target_base_path)
 
     if not target_base_path.exists():
         raise FileNotFoundError(f"Target base path '{target_base_path}' does not exist")
 
-    print(f"Scanning '{source_path}' for files...")
+    print(f"Scanning for files...")
     paths_to_copy = []
-    for path in source_path.glob("**/*"):
-        if path.is_file():
-            paths_to_copy.append(path)
+    for source_path in source_paths:
+        if source_path.is_file():
+            paths_to_copy.append(source_path)
+        else:
+            for path in source_path.glob("**/*"):
+                if path.is_file():
+                    paths_to_copy.append(path)
     size_to_copy = sum(path.stat().st_size for path in paths_to_copy)
     print(f"Found {len(paths_to_copy)} file(s) ({bytes_to_human_str(size_to_copy)}) to copy")
 
